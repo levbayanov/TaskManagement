a3b1149c84a4c426410267280eaf8a0fa2f73da8
Lev <levbayanov@gmail.com>
2024-11-01 14:15:43 +0500

    Добавил зависимсти в pom springSecurity и класс UserDetailsServiceImpl

---
+++ src/main/java/ru/LevBayanov/TaskManagement/servis/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package ru.LevBayanov.TaskManagement.servis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import ru.LevBayanov.TaskManagement.entity.UserEntity;
+import ru.LevBayanov.TaskManagement.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUserName(username);
+        return null;
+    }
+}
